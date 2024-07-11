bd1c83811bb4b9255e55a16344a517e1bc15d259
Nickson <lalatnickson@gmail.com>
2024-07-11 15:31:28 +0300

    string lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Nickson";
+const welcomeMessage = `Welcome to Flatbook, Nickson!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, NICKSON!`;
+const shortGreeting = `Welcome, N!`
